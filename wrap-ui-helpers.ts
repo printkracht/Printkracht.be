d571794c1b3b012330a2f394dad7a8f6731071ee
Printkracht <info@printkracht.be>
2025-08-12 15:22:44 +0200

    Delete wrap-ui-helpers.ts

--- wrap-ui-helpers.ts
+++
@@ -1,5 +0,0 @@
-import { estimateWrap as calc, WrapInput as WI } from "./pricing";
-export type { WrapInput, VehicleType } from "./pricing";
-export { WRAP_SETTINGS } from "./settings";
-export const estimateWrap = calc;
-export type WrapInput = WI;
